804a0425a566312fb8b455c7b70c244951b561bd
Rishanmehmi <rishanmehmi2@gmail.com>
2021-10-02 20:02:20 +0530

    1.0

---
+++ main.js
@@ -0,0 +1,95 @@
+function toggle() {
+    var element = document.getElementById("dropdown1");
+       var element2 = document.getElementById("dropdown2") ;
+       var element3 = document.getElementById("dropdown3") ;
+       var element4 = document.getElementById("dropdown4") ;
+       var element5 = document.getElementById("dropdown5") ;
+       var element6 = document.getElementById("dropdown6") ;
+       var element7 = document.getElementById("dropdown7") ;
+    
+      if (element) {
+        var display = element.style.display;
+    
+        if (display == "none") {
+            element.style.display = "block";
+            document.getElementById("block0").style.height = "529px";
+        } else {
+            element.style.display = "none";
+            document.getElementById("block0").style.height = "100px";
+        }
+      }
+      
+      if (element1) {
+        var display = element.style.display;
+    
+        if (display == "none") {
+            element1.style.display = "block";
+            document.getElementById("block0").style.height = "529px";
+        } else {
+            element1.style.display = "none";
+            document.getElementById("block0").style.height = "100px";
+        }
+      }
+      if (element2) {
+        var display = element.style.display;
+    
+        if (display == "none") {
+            element2.style.display = "block";
+            document.getElementById("block0").style.height = "529px";
+        } else {
+            element2.style.display = "none";
+            document.getElementById("block0").style.height = "100px";
+        }
+      }
+      if (element3) {
+        var display = element.style.display;
+    
+        if (display == "none") {
+            element3.style.display = "block";
+            document.getElementById("block0").style.height = "529px";
+        } else {
+            element3.style.display = "none";
+            document.getElementById("block0").style.height = "100px";
+        }
+      }
+      if (element4) {
+        var display = element.style.display;
+    
+        if (display == "none") {
+            element4.style.display = "block";
+            document.getElementById("block0").style.height = "529px";
+        } else {
+            element4.style.display = "none";
+            document.getElementById("block0").style.height = "100px";
+        }
+      }
+      if (element5) {
+        var display = element.style.display;
+    
+        if (display == "none") {
+            element5.style.display = "block";
+            document.getElementById("block0").style.height = "529px";
+        } else {
+            element5.style.display = "none";
+            document.getElementById("block0").style.height = "100px";
+        }
+      }
+      if (element6) {
+        var display = element.style.display;
+    
+        if (display == "none") {
+            element6.style.display = "block";
+            document.getElementById("block0").style.height = "529px";
+        } else {
+            element6.style.display = "none";
+            document.getElementById("block0").style.height = "100px";
+        }
+      }
+       
+    }
+    function slider()
+    { 
+        for(var i= 0; i< 3 ; i++){
+        document.getElementById.style.display = "block";
+        }
+    }
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Hello,
+Greetings of the day,
+Only A few breakpoints are left, first break point added.
+
